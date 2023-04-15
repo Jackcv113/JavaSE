bae6782c08e3f54315224ecbab32541d9f884ed2
jack <1094574789@qq.com>
2023-04-15 10:44:26 +0800

    Inner

--- day03/src/com/itheima/innerdemo1/Test.java
+++ day03/src/com/itheima/innerdemo1/Test.java
@@ -2,6 +2,13 @@ package com.itheima.innerdemo1;
 
 public class Test {
     public static void main(String[] args) {
+        Swimming s = new Swimming() {
+            @Override
+            public void swim() {
+                System.out.println("潜水10000m~~");
+            }
+        };
+        goSwim(s);
         //匿名内部类
         goSwim(new Swimming() {
             @Override

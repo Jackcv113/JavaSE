f3f47be7861396872360cdc09fcf13824f2c28cc
jack <1094574789@qq.com>
2023-04-15 11:42:09 +0800

    enum

--- day03/src/com/itheima/enumdemo/Choose.java
+++ day03/src/com/itheima/enumdemo/Choose.java
@@ -1,5 +1,5 @@
 package com.itheima.enumdemo;
-
+//枚举类作用:用来表示一组信息，然后作为参数传递,
 public enum Choose {
     BOY,GIRL
 }
--- day03/src/com/itheima/enumdemo/Test.java
+++ day03/src/com/itheima/enumdemo/Test.java
@@ -2,9 +2,10 @@ package com.itheima.enumdemo;
 
 public class Test {
     public static void main(String[] args) {
+        //使用枚举: 可以对传入的参数值进行限制
         command(Choose.BOY);
     }
-
+    //推荐方法，根据性别推荐对应的数据
     public static void command(Choose c){
         switch (c){
             case BOY -> System.out.println("给男生推荐的书");
